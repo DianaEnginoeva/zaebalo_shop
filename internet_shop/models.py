e318f2f2ccc82d6b28ea80c9dd78206578d37a3e
Your Name <you@example.com>
2018-01-16 19:24:08 +0300

    Maybe I will be something like programmer

---
+++ internet_shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    picture = models.TextField()
+    describe = models.TextField()
+    number_available = models.TextField()
+    price = models.TextField()
+    number_in_cart = models.TextField()
+    cost = models.TextField()
+    
+    
+def add_to_cart(self):
+        self.save()
+
+        
+def __str__(self):
+    return self.name
