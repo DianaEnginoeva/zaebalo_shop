7380f7b03cdf8e2b4c26c96930e904b985c37a1b
Your Name <you@example.com>
2018-01-17 22:11:40 +0300

    Almost done

--- internet_shop/urls.py
+++ internet_shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.cart_list, name='cart_list'),
+    url(r'^product/(?P<pk>\d+)/$', views.product_detail, name='product_detail'),
 ]
--- internet_shop/views.py
+++ internet_shop/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Product
+from django.shortcuts import render, get_object_or_404
 
 def cart_list(request):
     products = Product.objects.filter(cost=True).order_by('name')
     return render(request, 'internet_shop/cart_list.html', {'products': products})
 
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'internet_shop/product_detail.html', {'product': product})
